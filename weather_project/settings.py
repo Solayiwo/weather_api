7e3ae320712e3bb916e766bfd8c0b7965ed7723d
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-07-02 16:38:13 +0100

    Api connect

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WEATHER_API_KEY = 'cf9a97cfb5894c00b56123102240207'
