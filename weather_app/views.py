4107636ef4d444b3390f83af0cfab11ac0539112
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-07-03 01:51:30 +0100

    Update

--- weather_app/views.py
+++ weather_app/views.py
@@ -8,13 +8,11 @@ api_key = settings.WEATHER_API_KEY
 
 def get_client_ip(request):
     """Retrieve the client's IP address from the request"""
-    x_forwardee_for = request.META.get('HTTP_x_FORWARDED_FOR')
-    if x_forwardee_for:
-        ip = x_forwardee_for.split(',')
-        ip = ip[0]
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-
     return ip
 
 def get_location(ip):
